ef82fa26b608723ff18aef4863085dde8a973b76
Edvard Hübinette <edvard@hubinette.me>
2017-06-15 17:14:35 +0200

    Add linear functors

---
+++ src/Data/Functor/LFunctor.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+-- | A linear version of a Functor
+module Data.Functor.LFunctor where
+
+import Prelude (flip)
+
+class LFunctor f where
+    fmap :: (a ⊸ b) ⊸ f a ⊸ f b
+    (<$) :: a -> f b ⊸ f a
+
+(<$>) :: LFunctor f => (a ⊸ b) ⊸ f a ⊸ f b
+(<$>) = fmap
+
+infixl 4 $>
+
+($>) :: LFunctor f => f a ⊸ b -> f b
+($>) = flip (<$)
+
+void :: LFunctor f => f a -> f ()
+void x = () <$ x
