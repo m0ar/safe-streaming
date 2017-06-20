88c642e0b91d5ec91a8dfa0244299bb4ab7ada0e
Edvard Hübinette <edvard@hubinette.me>
2017-06-20 17:39:08 +0200

    Fix indentation and import cleanup of LMonad

--- src/Control/Monad/LMonad.hs
+++ src/Control/Monad/LMonad.hs
@@ -2,24 +2,23 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Control.Monad.LMonad (
-    LMonad,
-    (>>=), (>>),
-    return, fail
+  LMonad,
+  (>>=), (>>),
+  return, fail
 ) where
 
-import Control.Applicative (Applicative(), pure)
-import Data.Linear (const)
+import Data.Functor.LFunctor (LFunctor())
 import GHC.Base (String())
 
 infixl 1 >>, >>=
 
-class LMonad m where
-    (>>=) :: m a ⊸ (a ⊸ m b) ⊸ m b
+class LFunctor m => LMonad m where
+  (>>=) :: m a ⊸ (a ⊸ m b) ⊸ m b
     
-    (>>) :: m () ⊸ m b ⊸ m b
-    m >> k = m >>= \() -> k
-    {-# INLINE (>>) #-}
+  (>>) :: m () ⊸ m b ⊸ m b
+  m >> k = m >>= \() -> k
+  {-# INLINE (>>) #-}
 
-    return :: a ⊸ m a
+  return :: a ⊸ m a
 
-    fail :: String -> m a
+  fail :: String -> m a
