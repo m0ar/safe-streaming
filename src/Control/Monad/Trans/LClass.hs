f6e25918d8ef60c432e042e2e2ba4dba9a8f5d52
Edvard Hübinette <edvard@hubinette.me>
2017-06-22 16:21:48 +0200

    Add linear MonadTrans class

---
+++ src/Control/Monad/Trans/LClass.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Control.Monad.Trans.LClass (
+  LMonadTrans(..)
+) where
+
+import Control.Monad.LMonad (LMonad())
+
+class LMonadTrans t where
+  lift :: (LMonad m) => m a ⊸ t m a
