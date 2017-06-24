19adddb4acecdd97bb2ceb97ebf6aa080d110536
Edvard Hübinette <edvard@hubinette.me>
2017-06-24 13:07:49 +0200

    Add linear id and ($)

--- src/Data/Linear.hs
+++ src/Data/Linear.hs
@@ -3,9 +3,18 @@
 
 module Data.Linear where
 
+id :: a ⊸ a
+id a = a
+
 liftUnit :: a ⊸ () ⊸ a
 liftUnit a () = a 
 
 flip :: (a ⊸ b ⊸ c ) ⊸ b ⊸ a ⊸ c
 flip f b a = f a b
+
+infixr 0 $
+
+($) :: (a ⊸ b) ⊸ a ⊸ b
+f $ x = f x
+{-# INLINE ($) #-}
 
