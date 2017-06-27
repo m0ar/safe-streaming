a0945b62df6ff5d581dff49d3d3e2dd79484b12e
Edvard Hübinette <edvard@hubinette.me>
2017-06-27 12:15:55 +0200

    Add default implementation of <$

--- src/Data/Functor/LFunctor.hs
+++ src/Data/Functor/LFunctor.hs
@@ -4,11 +4,14 @@
 -- | A linear version of a Functor
 module Data.Functor.LFunctor where
 
-import Data.Linear (flip)
+import Data.Linear (flip, liftUnit)
+import Prelude ((.))
 
 class LFunctor f where
   fmap :: (a ⊸ b) ⊸ f a ⊸ f b
+
   (<$) :: a -> f () ⊸ f a
+  (<$) = fmap . liftUnit
 
 (<$>) :: LFunctor f => (a ⊸ b) ⊸ f a ⊸ f b
 (<$>) = fmap
@@ -19,5 +22,3 @@ class LFunctor f where
 infixl 4 $>
 infixl 4 <$
 infixl 4 <$>
-
-
