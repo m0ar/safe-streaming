d1cdc0c3357da50588e60241cc79748736bcb818
Edvard Hübinette <edvard@hubinette.me>
2017-07-28 16:00:54 +0200

    Change the LState type, add LFunctor instance

--- src/Control/Monad/State/LState.hs
+++ src/Control/Monad/State/LState.hs
@@ -1,11 +1,30 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Control.Monad.State.LState where
 
--- | A linear state function for some state @s@.
-type LState s a = s ⊸ (s, a)
+import Control.Monad.LMonad
+import Control.Applicative.LApplicative
+import Data.Functor.LFunctor
 
+-- | A linear state function on some state @s@.
+newtype LState s a = LState { runLState :: s ⊸ (s, a) }
+
+instance LFunctor (LState s) where
+  fmap :: forall s a b. (a ⊸ b) ⊸ LState s a ⊸ LState s b
+  fmap f (LState cont) = LState $ \s -> apply $ cont s
+    where
+      apply :: (s, a) ⊸ (s, b)
+      apply (newSt, a) = (newSt, f a)
+
+--instance LApplicative (LState s) where
+
+--instance LMonad (LState s) where
+--  return :: a ⊸ LState s a
+--  return x = LState $ \s -> (s, x)
 
 -- | A fully linear type of state.
 --
@@ -37,27 +56,30 @@ data PLS a b where PLS :: a ⊸ b -> PLS a b
 -- This can be done regardless of linearity in the state, because it threads
 -- it linearly and returns nothing.
 modify :: (s ⊸ s) ⊸ LState s ()
-modify fn s = (fn s, ())
+modify fn = LState $ \s -> (fn s, ())
 
 
--- | Type class for defining what @get@:ing your state type means,
--- since this depends on what it contains that is unrestricted.
+-- | Type class for defining what @get@:ing your state type means, since this
+-- depends on what it contains that is unrestricted.
 class Gettable s where
   type GetRes s :: *
   get :: LState s (GetRes s)
 
+
 -- | From a fully linear state, @get@ can only return @()@.
 instance Gettable (LS a) where
   type GetRes (LS a) = ()
-  get s = (s, ())
+  get = LState $ \s -> (s, ())
+
 
 -- | From an unrestricted state, @get@ can return the whole state.
 instance Gettable (S a) where
   type GetRes (S a) = a
-  get (S a) = (S a, a)
+  get = LState $ \(S a) -> (S a, a)
+
 
 -- | From a partially linear state, @get@ can return the unrestricted
 -- components.
 instance Gettable (PLS a b) where
   type GetRes (PLS a b) = b
-  get (PLS a b) = (PLS a b, b)
+  get = LState $ \(PLS a b) -> (PLS a b, b)
